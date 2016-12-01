d0c7c6dfb382303dbdc7f8d0beff9b89d75c2a4a
taddyb <tadd.bindas1@marist.edu>
2016-11-30 20:22:05 -0500

    Lab 10, Tadd Bindas, 11/30/2016, Stored Procedures!

---
+++ lab-10.sql
@@ -0,0 +1,62 @@
+﻿
+select *
+from Prerequisites
+order by courseNum DESC;
+
+----------------------------------------------------
+----------------------------------------------------
+
+create or replace function PreReqsFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   courseNumber int 	:=1;
+   resultset REFCURSOR 	:=2;
+begin
+   open resultset for 
+	select preReqNum
+	from Prerequisites 
+	where courseNum = courseNumnber;
+   return resultset;
+  end;
+  $$
+  language plpgsql;
+
+  -------------------------------------
+ create or replace function IsPreReqFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   courseNumber int 	:=1;
+   resultset REFCURSOR 	:=2;
+begin
+   open resultset for 
+	select courseNum
+	from Prerequisites 
+	where preReqNum = 308;
+   return resultset;
+  end;
+  $$
+  language plpgsql;
+   -------------------------------------
+
+   create or replace function Jedi(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   courseNumber int 	:=1;
+   resultset REFCURSOR 	:=2;
+begin
+	if PreReqsFor(courseNumber, 'results')
+	then execute procedure;
+	Else
+	Rollback;
+	End if;
+end;
+$$
+language plpgsql;
+----------------------------------------------------
+----------------------------------------------------
+
+select PreReqsFor(499, 'results');
+Fetch all from results;
+
+select IsPreReqFor(499, 'results');
+Fetch all from results;
